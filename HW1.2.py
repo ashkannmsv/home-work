5f894c2ea6eebb779ac86a722ea8e790a9a819c5
arminsohrabizadeh007-eng <arminsohrabizadeh007@gmail.com>
2025-12-04 00:05:33 +0330

    my secend home work

---
+++ HW1.2.py
@@ -0,0 +1,4 @@
+fname = input("inter your ferst name:")
+lname = input("inter your last name:")
+age = int(input("inter youe age:"))
+print(F"your ferst name is {fname} and your last name is {lname} and yor age is {age}")
