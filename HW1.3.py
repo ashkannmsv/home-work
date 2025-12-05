267e92bb2159cff254fc68b0d7b99bb3548f5a07
arminsohrabizadeh007-eng <arminsohrabizadeh007@gmail.com>
2025-12-05 18:16:20 +0330

    armins 1.3 home work

---
+++ HW1.3.py
@@ -0,0 +1,12 @@
+rectangle_length = int(input("Enter the rectangle length:"))
+rectangle_width = int(input("Enter the rectangle width:"))
+S = (rectangle_length * rectangle_width)
+P = (rectangle_length + rectangle_width) * 2
+print(F"the Perimeter is: {P} and the Area is: {S}")
+Colaw_1 = rectangle_length > rectangle_width
+print(F"The comparison is that the length is greater than the width: {Colaw_1}")
+Colaw_2 = rectangle_length == rectangle_width
+print(F"The comparison of the equality of length and width is: {Colaw_2}")
+FNAME = input("Enter your name:")
+S *= 2
+print(F"yuor name is: {FNAME} and the new area is: {S} and the length of rectangle is: {rectangle_length} and the width is: {rectangle_width} GOD LUCK")
